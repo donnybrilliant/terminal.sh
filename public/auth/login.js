61d4a2039f80260498f43ed5c0b99fdf5c7c9422
donnybrilliant <37931601+donnybrilliant@users.noreply.github.com>
2024-06-09 17:31:03 +0200

    Fix login for jwt

--- public/auth/login.js
+++ public/auth/login.js
@@ -1,9 +1,10 @@
-import { loadFileSystem } from "../terminal/fileSystem.js";
 import { fetchWithTimeout } from "../utils/fetch.js";
 
 export class LoginManager {
-  constructor(apiUrl) {
+  constructor(socket, apiUrl) {
+    this.socket = socket;
     this.apiUrl = apiUrl;
+    this.username = null;
   }
 
   setTerminal(term) {
@@ -11,99 +12,83 @@ export class LoginManager {
   }
 
   setUsername(username) {
-    sessionStorage.setItem("username", username);
+    this.username = username;
   }
 
   getUsername() {
-    return sessionStorage.getItem("username") || "";
-  }
-
-  clearUsername() {
-    sessionStorage.removeItem("username");
+    return this.username;
   }
 
   async initializeLoginState() {
-    try {
-      const status = await this.checkAuthStatus();
-      if (status.data.authenticated) {
-        this.setUsername(status.data.user.username);
-        await loadFileSystem(this.apiUrl);
-        // return this?
-        console.log(`Logged in as ${status.data.user.username}`);
-      } else {
-        await loadFileSystem(this.apiUrl);
-      }
-    } catch (error) {
-      console.error(`Failed to check login status: ${error.message}`);
+    this.socket.connect(); // Connect as a guest initially
+    const token = localStorage.getItem("jwtToken");
+    if (token) {
+      await this.authenticateSocket(token);
     }
   }
 
+  async authenticateSocket(token) {
+    const response = await this.emitSocketEvent("authenticate", token);
+    if (response.success) {
+      this.setUsername(response.user.username);
+      console.log(`Authenticated as ${response.user.username}`);
+    } else {
+      console.log(response.message);
+      localStorage.removeItem("jwtToken");
+      throw new Error(response.message);
+    }
+    // load file system?
+  }
+
+  emitSocketEvent(event, data) {
+    return new Promise((resolve, reject) => {
+      this.socket.emit(event, data, (response) => {
+        if (response) {
+          resolve(response);
+        } else {
+          reject(new Error("No response from socket event"));
+        }
+      });
+    });
+  }
+
   async login(username, password) {
+    const token = localStorage.getItem("jwtToken");
+    if (token) {
+      this.term.write(`\r\nAlready logged in.\r\n`);
+      return;
+    }
+
     try {
-      const status = await this.checkAuthStatus();
-      if (status.data.authenticated) {
-        this.term.write(
-          `\r\nUser already logged in as ${status.data.user.username}\r\n`
-        );
+      const result = await fetchWithTimeout(`${this.apiUrl}/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      if (result.success) {
+        const { token, user } = result.data;
+        localStorage.setItem("jwtToken", token);
+        await this.authenticateSocket(token);
+        this.term.write(`\r\n${result.message}\r\n`);
       } else {
-        const result = await this.authenticateUser(username, password);
         this.term.write(`\r\n${result.message}\r\n`);
       }
     } catch (error) {
       console.error(`Failed to log in: ${error.message}`);
-      this.term.write(
-        `\r\nUnable to verify login status. Please try logging in again.\r\n`
-      );
-    }
-  }
-
-  async authenticateUser(username, password) {
-    try {
-      const data = await fetchWithTimeout(`${this.apiUrl}/login`, {
-        method: "POST",
-        body: JSON.stringify({ username, password }),
-      });
-      if (data.success) {
-        this.setUsername(username);
-        await loadFileSystem(this.apiUrl);
-      }
-      return data;
-    } catch (error) {
-      return error;
+      this.term.write(`\r\nFailed to log in: ${error.message}\r\n`);
     }
   }
 
   async logout() {
-    try {
-      const status = await this.checkAuthStatus();
-      if (status.data.authenticated) {
-        const data = await fetchWithTimeout(`${this.apiUrl}/logout`, {
-          method: "POST",
-        });
-        this.clearUsername();
-        await loadFileSystem(this.apiUrl);
-        this.term.write(`\r\n${data.message}\r\n`);
-      } else {
-        this.term.write(`\r\nYou are not logged in.`);
-      }
-    } catch (error) {
-      this.term.write(`\r\nError logging out: ${error.message}\r\n`);
-    }
-  }
-
-  async checkAuthStatus() {
-    try {
-      return await fetchWithTimeout(`${this.apiUrl}/auth-status`);
-    } catch (error) {
-      this.term.write(
-        `\r\nError checking authentication status: ${error.message}\r\n`
-      );
-      console.error(
-        `Silent check for authentication status failed: ${error.message}`
-      );
-
-      throw error;
-      //return { data: { authenticated: false } }; // Provide a default response to handle error states gracefully
-    }
+    localStorage.removeItem("jwtToken");
+    await this.emitSocketEvent("authenticate", null);
+    this.socket.auth = {};
+    this.setUsername(null);
+    this.term.write(`\r\nLogged out successfully.\r\n`);
+    this.socket.disconnect();
+    await this.initializeLoginState();
   }
 }
